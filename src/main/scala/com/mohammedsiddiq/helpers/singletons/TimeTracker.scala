6521aa7fd598e8e213240fccb714d6764117012f
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-10-01 22:00:08 -0500

    Time tracker class to track the exection time

---
+++ src/main/scala/com/mohammedsiddiq/helpers/singletons/TimeTracker.scala
@@ -0,0 +1,12 @@
+package com.mohammedsiddiq.helpers.singletons
+
+/**
+  * Keeps track of execution time for each query
+  */
+object TimeTracker {
+
+  var startTime: Long = 0L
+  var endTime: Long = 0L
+  var totalTime: Long = 0L
+
+}
